fb7958e659ea02f33a7d40607c42d7a39c9d6ecf
leasorssini <dlegasr@gmail.com>
2024-12-02 18:47:29 -0300

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -42,14 +42,3 @@ resource "aws_instance" "app" {
     Name = "app-instance"
   }
 }
-
-# Outputs
-output "instance_id" {
-  description = "ID da instância EC2 criada"
-  value       = aws_instance.app.id
-}
-
-output "instance_public_ip" {
-  description = "Endereço IP público da instância EC2"
-  value       = aws_instance.app.public_ip
-}
