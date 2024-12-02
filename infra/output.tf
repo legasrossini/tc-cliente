f700703df43bbecc5ca98cd0515af8d6ed80153e
leasorssini <dlegasr@gmail.com>
2024-12-02 18:30:09 -0300

    teste

--- infra/output.tf
+++ infra/output.tf
@@ -1,4 +1,9 @@
 output "instance_id" {
-  description = "ID da instância EC2"
-  value = aws_spot_instance_request.app.id
+  description = "ID da instância EC2 criada"
+  value       = aws_instance.app.id
+}
+
+output "instance_public_ip" {
+  description = "Endereço IP público da instância EC2"
+  value       = aws_instance.app.public_ip
 }
