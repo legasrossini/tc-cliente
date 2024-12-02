d54785b6c75a78bc40977900478e521ff645ce13
leasorssini <dlegasr@gmail.com>
2024-12-02 04:25:34 -0300

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -21,3 +21,48 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "source_instance" {
+  ami           = var.base_ami_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "SourceInstance"
+  }
+}
+
+resource "aws_ami_from_instance" "app_ami" {
+  name               = "app-ami"
+  source_instance_id = aws_instance.source_instance.id
+  tags = {
+    Name = "AppAMI"
+  }
+}
+
+resource "aws_spot_instance_request" "app" {
+  ami           = aws_ami_from_instance.app_ami.id
+  instance_type = "t3.micro"
+  spot_price    = "0.005"
+
+  tags = {
+    Name = "tc-cliente"
+  }
+
+  security_groups = length(data.aws_security_group.existing_sg.id) > 0 ? [data.aws_security_group.existing_sg.id] : [aws_security_group.app_sg.id]
+}
