78b8ec80042c4447c6e718eba30a39be3fd1c75d
leasorssini <dlegasr@gmail.com>
2024-12-03 00:38:15 -0300

    teste

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -22,4 +22,53 @@ resource "aws_api_gateway_resource" "my_resource" {
 }
 
 # Método GET
-resource "aws_api_gateway_method" "get_m
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  type        = "HTTP"
+  uri         = "http://${data.aws_instance.app.public_ip}:8080/cliente"
+  depends_on  = [aws_instance.app]
+}
+
+# Método POST
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  type        = "HTTP"
+  uri         = "http://${data.aws_instance.app.public_ip}:8080/cliente"
+  depends_on  = [aws_instance.app]
+}
+
+resource "aws_api_gateway_deployment" "my_deployment" {
+  depends_on  = [
+    aws_api_gateway_integration.get_integration,
+    aws_api_gateway_integration.post_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.my_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  stage_name    = "prod"
+}
+
+output "api_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}/cliente"
+}
